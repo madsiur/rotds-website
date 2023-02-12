55757397bb38cb2d9155d0b70ff51b6d6e55e38f
Toussaint-Dupont, Frédéric <toussaint-dupont.frederic@courrier.uqam.ca>
2023-02-12 01:59:02 -0500

    Use of SVG instead of image

--- website/js/ost.js
+++ website/js/ost.js
@@ -1,7 +1,8 @@
 function listSongTable(array, dirName) {
     var table = $('#ostTbl');
     var counter = 1;
-    var playImg = '<img src="images/play-circle-fill.svg" class="play-img" />'
+    var playImg = '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-play-circle-fill play-img" viewBox="0 0 16 16">';
+    playImg += '<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/></svg>';
     
     $.each(array, function(key,value) {
         // create duration string
